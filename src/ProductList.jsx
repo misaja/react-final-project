777e02b23853ba3009710eec0b8089fcd06dee51
misaja <mauro.isaja@gmail.com>
2024-08-13 18:56:56 +0200

    ok, this is really the final commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -289,7 +289,7 @@ function ProductList() {
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         &nbsp;&nbsp;&nbsp;
-                        <a href="/" style={{ textDecoration: 'none' }}>
+                        <a href="/react-final-project/" style={{ textDecoration: 'none' }}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
